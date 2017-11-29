ac1e328533cb92fdf2823b0b22e5dbe7b12accdd
vishalmanani <vishal@lintelindia.com>
2017-11-29 10:27:15 +0530

    first commit

---
+++ biba/environ.py
@@ -0,0 +1,7 @@
+DEBUG = True
+LISTEN_HOST = 'localhost'
+LISTEN_PORT = 8000
+APP_URL = 'http://localhost:8000'
+APP_CLIENT_ID = 'hrc4ek4ze2p77enpe08dq56plmdz6z8'
+APP_CLIENT_SECRET = 'oam9jfaj4n7olb6gnxetiza5jozt83h'
+SESSION_SECRET = ''
