04f643ca4d00237972ef3a8b850cfe2051779d97
vishalmanani <vishal@lintelindia.com>
2017-12-10 19:22:42 +0530

    test db

--- display/models.py
+++ display/models.py
@@ -24,7 +24,7 @@ class User(models.Model):
 class StoreUser(models.Model):
     store_id = models.ForeignKey(Store, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    admin = models.BooleanField(default=True)
+    admin = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.store_id)
