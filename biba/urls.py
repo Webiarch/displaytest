d93de3b516e3cad5acff7a4f8b3894968b02186c
vishalmanani <vishal@lintelindia.com>
2017-12-10 23:26:49 +0530

    test db

--- biba/urls.py
+++ biba/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     url(r'^admin', admin.site.urls),
     url(r'^bigcommerce/callback', views.AuthCallback.as_view(), name='authcallback'),
     url(r'^bigcommerce/load', views.Load.as_view(), name='load'),
-    url(r'/', views.Display.as_view(), name='display'),
+    url(r'^/', views.Display.as_view(), name='display'),
 
 ]
