af587fbd7e6bfc2ba41c71ac252da99ddbd252cd
vishalmanani <vishal@lintelindia.com>
2017-12-10 18:13:44 +0530

    test db

--- display/models.py
+++ display/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Store(models.Model):
-    store_hash = models.CharField(max_length=100, null=True, blank=True)
-    access_token = models.CharField(max_length=100, null=True, blank=True)
-    scope = models.CharField(max_length=100, null=True, blank=True)
+    store_hash = models.CharField(max_length=1000, null=True, blank=True)
+    access_token = models.CharField(max_length=1000, null=True, blank=True)
+    scope = models.CharField(max_length=1000, null=True, blank=True)
     admin_storeuser_id = models.IntegerField(null=True, blank=True, default=True)
     storeusers = models.ManyToManyField('StoreUser',)
 
@@ -14,7 +14,7 @@ class Store(models.Model):
 
 class User(models.Model):
     bc_id = models.IntegerField(null=True, blank=True)
-    email = models.CharField(max_length=100, null=True, blank=False)
+    email = models.CharField(max_length=1000, null=True, blank=False)
     storeusers = models.ManyToManyField('StoreUser',)
 
     def __str__(self):
