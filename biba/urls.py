a49c1e130e96f1eaf9c855baab0ba86c5b616c3d
vishalmanani <vishal@lintelindia.com>
2017-12-11 17:33:40 +0530

    test db

--- biba/urls.py
+++ biba/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from display import views
 
 urlpatterns = [
-    url(r'^', views.Display.as_view(), name='display'),
     url(r'^admin', admin.site.urls),
     url(r'^bigcommerce/callback', views.AuthCallback.as_view(), name='authcallback'),
     url(r'^bigcommerce/load', views.Load.as_view(), name='load'),
+    url(r'', views.Display.as_view(), name='display'),
 ]
